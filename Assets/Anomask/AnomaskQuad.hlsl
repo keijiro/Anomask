b746164e05194b5ede62272a072da433de257e7b
Keijiro Takahashi <keijiro@gmail.com>
2025-10-25 14:43:30 +0900

    Tweaks

--- Assets/Anomask/AnomaskQuad.hlsl
+++ Assets/Anomask/AnomaskQuad.hlsl
@@ -8,7 +8,7 @@ void AnomaskQuadMain_float
 {
     float2 uv = BaseUV + (UV - 0.5) * UVScale;
     float r = tex2D(SourceTex, uv + float2(Offset.x, 0)).r;
-    float g = tex2D(SourceTex, uv + float2(Offset.y, 0)).g;
-    float b = tex2D(SourceTex, uv + float2(Offset.z, 0)).b;
+    float g = tex2D(SourceTex, uv + float2(Offset.x + Offset.y, 0)).g;
+    float b = tex2D(SourceTex, uv + float2(Offset.x + Offset.z, 0)).b;
     Output = float3(r, g, b);
 }
